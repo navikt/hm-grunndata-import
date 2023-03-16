24b748e135e4c3a9b3f579be32bdc596d5f001d5
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-16 13:19:40 +0100

    upgrade libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,20 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion="3.8.0"
-val kafkaVersion = "3.2.1"
+val micronautVersion="3.8.6"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.0"
 val jacksonVersion = "2.13.4"
 val logbackClassicVersion = "1.4.5"
-val logbackEncoderVersion = "7.2"
-val kafkaEmbeddedVersion = "3.2.1"
-val postgresqlVersion= "42.5.1"
+val logbackEncoderVersion = "7.3"
+val postgresqlVersion= "42.5.4"
 val tcVersion= "1.17.6"
-val mockkVersion = "1.13.2"
-val kotestVersion = "5.5.4"
+val mockkVersion = "1.13.4"
+val kotestVersion = "5.5.5"
 val apachePoiVersion = "5.2.3"
-val openSearchRestClientVersion = "1.3.5"
+val openSearchRestClientVersion = "2.4.1"
+val rapidsRiversVersion="202303011052"
+val grunndataDtoVersion = "202303081356"
 
 group = "no.nav.hm"
 version = properties["version"] ?: "local-build"
@@ -26,7 +26,7 @@ plugins {
     kotlin("plugin.allopen") version "1.7.0"
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.micronaut.application") version "3.6.6"
+    id("io.micronaut.application") version "3.7.2"
 }
 
 configurations.all {
@@ -45,14 +45,14 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
     // security
     implementation("io.micronaut.security:micronaut-security-jwt")
-    annotationProcessor("io.micronaut.security:micronaut-security-annotations")
+    kapt("io.micronaut.security:micronaut-security-annotations")
 
-    // micronaut-data
+    kapt("io.micronaut.data:micronaut-data-processor")
     implementation("io.micronaut.data:micronaut-data-jdbc")
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("io.micronaut.sql:micronaut-jdbc-hikari")
-    annotationProcessor("io.micronaut.data:micronaut-data-processor")
+
     implementation("org.postgresql:postgresql:${postgresqlVersion}")
     implementation("io.micronaut.flyway:micronaut-flyway")
     implementation("io.micronaut:micronaut-runtime")
@@ -63,13 +63,17 @@ dependencies {
     implementation("io.micronaut.micrometer:micronaut-micrometer-core")
     implementation("io.micronaut.micrometer:micronaut-micrometer-registry-prometheus")
     implementation("io.micronaut:micronaut-management")
-    // Apache POI for excel file handling
-    implementation("org.apache.poi:poi:$apachePoiVersion")
+
+    // Rapids and Rivers
+    implementation("com.github.navikt:hm-rapids-and-rivers-v2-core:$rapidsRiversVersion")
+    implementation("com.github.navikt:hm-rapids-and-rivers-v2-micronaut:$rapidsRiversVersion")
+    implementation("no.nav.hm.grunndata:hm-grunndata-rapid-dto:$grunndataDtoVersion")
+
     testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation("io.micronaut.test:micronaut-test-kotest5")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testImplementation("org.testcontainers:postgresql:${tcVersion}")
 }
 
@@ -119,6 +123,7 @@ tasks.withType<Wrapper> {
 repositories {
     mavenLocal()
     mavenCentral()
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
     maven("https://packages.confluent.io/maven/")
 
 }
