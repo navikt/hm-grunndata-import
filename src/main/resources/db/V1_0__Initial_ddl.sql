eb38049d90adcd732de82d587ab7db4bfc4c54a5
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-17 13:01:52 +0100

    fix db error

--- src/main/resources/db/V1_0__Initial_ddl.sql
+++ src/main/resources/db/V1_0__Initial_ddl.sql
@@ -15,11 +15,12 @@ CREATE TABLE IF NOT EXISTS supplier_v1 (
 
 CREATE TABLE IF NOT EXISTS transferstate_v1 (
     id uuid NOT NULL PRIMARY KEY,
+    product_id uuid NOT NULL,
     supplier_id uuid NOT NULL,
     reference VARCHAR(255) NOT NULL,
     md5 VARCHAR(32) NOT NULL,
     json_payload JSONB NOT NULL,
-    status VARCHAR(32) NOT NULL,
+    transfer_status VARCHAR(32) NOT NULL,
     message TEXT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
