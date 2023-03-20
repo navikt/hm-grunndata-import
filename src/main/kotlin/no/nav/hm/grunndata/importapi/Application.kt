27babe541460f46906cf218fd74c9c038ae4e6a7
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-20 13:05:52 +0100

    added open api definition

--- src/main/kotlin/no/nav/hm/grunndata/importapi/Application.kt
+++ src/main/kotlin/no/nav/hm/grunndata/importapi/Application.kt
@@ -1,7 +1,18 @@
 package no.nav.hm.grunndata.importapi
 
 import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 
+@OpenAPIDefinition(
+    info = Info(
+        title = "Hjelpemiddel API",
+        version = "0.1",
+        description = "NAVs Api for transferring accessibility devices"
+    )
+)
 object Application {
     @JvmStatic
     fun main(args: Array<String>) {
