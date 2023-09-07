00cc362b1b344852260da159cde9f04f5a79a93c
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-09-07 14:58:57 +0200

    sync from registration approve flow

--- src/main/kotlin/no/nav/hm/grunndata/importapi/adminstate/AdminStatusSyncRiver.kt
+++ src/main/kotlin/no/nav/hm/grunndata/importapi/adminstate/AdminStatusSyncRiver.kt
@@ -18,12 +18,13 @@ import no.nav.hm.grunndata.rapid.event.RapidApp
 import no.nav.hm.rapids_rivers.micronaut.RiverHead
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
+import java.util.*
 
 @Context
 @Requires(bean = KafkaRapid::class)
 class AdminStatusSyncRiver(river: RiverHead,
                            private val objectMapper: ObjectMapper,
-                           productAdminStateRepository: ProductAdminStateRepository
+                           private val productAdminStateRepository: ProductAdminStateRepository
 ): River.PacketListener {
 
     companion object {
@@ -34,6 +35,8 @@ class AdminStatusSyncRiver(river: RiverHead,
         river
             .validate { it.demandValue("createdBy", RapidApp.grunndata_register)}
             .validate { it.demandValue("eventName", EventName.registeredProductV1)}
+            .validate { it.demandKey("transferId")}
+            .validate { it.demandKey( "version")}
             .validate { it.demandKey("payload")}
             .validate { it.demandKey("dtoVersion")}
             .register(this)
@@ -41,13 +44,30 @@ class AdminStatusSyncRiver(river: RiverHead,
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         val dtoVersion = packet["dtoVersion"].asLong()
+        val version = packet["version"].asLong()
+        val transferId = UUID.fromString(packet["transferId"].textValue())
         if (dtoVersion > rapidDTOVersion)
             LOG.warn("this event dto version $dtoVersion is newer than our version: $rapidDTOVersion")
         val dto = objectMapper.treeToValue(packet["payload"], ProductRegistrationRapidDTO::class.java)
         if (DraftStatus.DONE == dto.draftStatus && IMPORT == dto.productDTO.createdBy) {
             runBlocking {
-                LOG.info("adminstatus sync for ${dto.id} with adminstatus ${dto.adminStatus}")
-
+                LOG.info("adminstatus sync for ${dto.id} with adminstatus ${dto.adminStatus} " +
+                        "with transferId $transferId and version: $version ")
+                val adminState = productAdminStateRepository.findById(dto.id)!!
+                if (adminState.version == version) {
+                    productAdminStateRepository.update(
+                        adminState.copy(
+                            productStatus = dto.productDTO.status,
+                            adminStatus = dto.adminStatus,
+                            adminMessage = dto.message,
+                            updated = LocalDateTime.now()
+                        )
+                    )
+                }
+                else {
+                    LOG.info("Registration version $version is not the same as admin state version ${adminState.version} " +
+                            "will skip this")
+                }
             }
         }
     }
