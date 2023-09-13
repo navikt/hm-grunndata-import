b3ad867500581ad7f256a3bf453716aee700c019
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-09-13 09:23:56 +0200

    added tests for token service

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## Development
 
-create a new test supplier: 
+Create a new test supplier: 
 ```
 insert into supplier_v1(id, name, status, identifier,jwtid,updated_by,created_by) values (gen_random_uuid(), 'test supplier', 'ACTIVE', gen_random_uuid(), gen_random_uuid(), 'IMPORT', 'IMPORT');
+```
 
+A test token, can be used in localhost:
+```
+eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Zpbm5oamVscGVtaWRsZXIubmF2Lm5vIiwic3ViIjoiQWRtaW4gVG9rZW4iLCJpYXQiOjE2OTQ1ODk3NTEsImp0aSI6IjRiMjUzYWVkLWZhMGQtNDhiNy04NGFjLTU0MDgyMWFjNzgwYiIsInJvbGVzIjoiUk9MRV9BRE1JTiJ9.FWdilcw-hBRpv5Vnt3O7rShDoDLEf77bFARSyGxatLc
 ```
--- src/test/kotlin/no/nav/hm/grunndata/importapi/security/TokenAPIControllerTest.kt
+++ src/test/kotlin/no/nav/hm/grunndata/importapi/security/TokenAPIControllerTest.kt
@@ -1,4 +1,34 @@
 package no.nav.hm.grunndata.importapi.security
 
-class TokenAPIControllerTest {
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import kotlinx.coroutines.runBlocking
+import no.nav.hm.grunndata.importapi.supplier.Supplier
+import no.nav.hm.grunndata.importapi.supplier.SupplierService
+import org.junit.jupiter.api.Test
+import java.util.*
+
+
+@MicronautTest
+class TokenAPIControllerTest(private val tokenAPIClient: TokenAPIClient,
+                             private val tokenService: TokenService,
+                             private val supplierService: SupplierService) {
+
+    val supplierId: UUID = UUID.randomUUID()
+    init {
+        runBlocking {
+            supplierService.save(
+                Supplier(id= supplierId, name = UUID.randomUUID().toString(),
+                identifier = UUID.randomUUID().toString(), jwtid = UUID.randomUUID().toString())
+            )
+        }
+    }
+    @Test
+    fun tokenApiTest() {
+        val bearerToken = "bearer ${tokenService.adminToken("tester")}"
+        val supplierToken = tokenAPIClient.createSupplierToken(supplierId, bearerToken)
+        val adminToken = tokenAPIClient.createAdminToken("Admin Token", bearerToken)
+        supplierToken.shouldNotBeNull()
+        adminToken.shouldNotBeNull()
+    }
 }
