2c7b678416d7b336d02940784d6e0484e19c9439
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-10-02 12:50:05 +0200

    setup docker-compose for importapi

--- src/test/resources/postgres/postgres-init.sql
+++ src/test/resources/postgres/postgres-init.sql
@@ -1,2 +1,10 @@
+CREATE USER gdb with password 'gdb';
+CREATE DATABASE gdb owner gdb;
+CREATE USER register with password 'register';
+CREATE DATABASE register owner register;
+CREATE USER media with password 'media';
+CREATE DATABASE media owner media;
 CREATE USER importapi with password 'importapi';
 CREATE DATABASE importapi owner importapi;
+CREATE USER mediaproxy WITH password 'mediaproxy';
+CREATE DATABASE mediaproxy owner mediaproxy;
